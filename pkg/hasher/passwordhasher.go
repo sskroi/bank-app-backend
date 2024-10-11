46fcd6af3e6f891d5d6e7547c2dda3ac2655e2d8
Andrei <sskroi200ok@gmail.com>
2024-10-11 22:19:46 +0300

    add PasswordHasher interface + BcryptHasher

---
+++ pkg/hasher/passwordhasher.go
@@ -0,0 +1,32 @@
+package hasher
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type PasswordHasher interface {
+	Hash(password string) (string, error)
+	Check(hashedPassowrd, password string) bool
+}
+
+type BcryptHasher struct {
+	cost int
+}
+
+func NewBcryptHasher() *BcryptHasher {
+	return &BcryptHasher{cost: bcrypt.DefaultCost}
+}
+
+// max len 72 bytes
+func (h *BcryptHasher) Hash(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), h.cost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func (h *BcryptHasher) Check(hashedPassword, password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)) == nil
+}
